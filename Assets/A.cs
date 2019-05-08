ecc29f7bd8de5d4278b09fdb48069fd7531eb350
Urielox <urielox2011@hotmail.com>
2019-05-08 01:18:12 -0500

    version ejecutable

---
+++ Assets/A.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class A : MonoBehaviour
+{
+    public void CambiarEscena(string nombre)
+    {
+        SceneManager.LoadScene(nombre);
+    }
+    public void CambiarEscena1(string Menu)
+    {
+        SceneManager.LoadScene(Menu);
+    }
+    public void SALIR(string nombre)
+    {
+        Application.Quit();
+    }
+}
